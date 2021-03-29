d2e21acb77d9979a35f60b89eaf11ba145565dbc
Macrelle_T <65347962+FlowWars@users.noreply.github.com>
2021-03-29 20:05:54 +0200

    mise a jour 1
    -minsize
    -ajout de dsn

--- projetStage.py
+++ projetStage.py
@@ -14,18 +14,25 @@ fenetre.title('Application d\'extraction')
 fenetre.geometry('600x600')
 fenetre.configure(bg='#36A679')
 
+fenetre.minsize(480,360)
+
 CheminFile = StringVar()
 
-def set_file():
-    CheminFile.set(askopenfilename(initialdir ="/", filetypes = [ ("all files", "*.*")]))
+def dsn():
+    CheminFile.set("connexion en cours")
+    conn = pypyodbc.connect('DSN=test;')  # initialisation de la connexion au serveur
+    cur = conn.cursor()
+    CheminFile.set("connexion ok")
     
-barre = Entry(fenetre, textvariable=CheminFile, width=50)
+barre = Label(fenetre, textvariable=CheminFile)
 barre.pack()
 barre.place(relx=0.5, rely=0.5, anchor=CENTER)
 
-btn_recherche = Button(fenetre, text='Parcourir', bg='black', fg='white', command=lambda : set_file())
+btn_recherche = Button(fenetre, text='Parcourir', bg='black', fg='white', command=lambda : dsn())
 btn_recherche.pack()
 btn_recherche.place(relx=0.5, rely=0.55, anchor=CENTER)
 
 btn_next = Button(fenetre, text='Suivant', bg='black', fg='white')
 btn_next.pack()
+
+fenetre.mainloop()
