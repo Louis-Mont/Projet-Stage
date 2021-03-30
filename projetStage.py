cda5474859ad52dd202e7a3dd76acb6106452183
Macrelle_T <65347962+FlowWars@users.noreply.github.com>
2021-03-30 15:38:06 +0200

    commit du code
    - ajout de l'extraction des codes d'insee

--- projetStage.py
+++ projetStage.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter.filedialog import askopenfilename
 import pypyodbc
+import datetime
+import csv
 
 fenetre =Tk() # initialisation de la fenetre
 fenetre.title('Connexion a GDR') # nom de la fenetre
@@ -11,21 +13,21 @@ fenetre.configure(bg='#36A679') # couleur du fond
 fenetre.minsize(480,360) # taille minimum de la fenetre
 
 CheminFile = StringVar() # déclaration de la variable string
-CheminFile.set("Veuillez vous connectez a GDR")
 
-# fonction qui permet de se connecter à la base GDR
-def dsn():
-    CheminFile.set("connexion en cours")
-    conn = pypyodbc.connect('DSN=GDR;')  # initialisation de la connexion au serveur
-    cur = conn.cursor()
-    CheminFile.set("connexion ok") 
+CheminFile.set("connexion en cours")
+conn = pypyodbc.connect('DSN=GDR;')  # initialisation de la connexion au serveur
+cur = conn.cursor()
+CheminFile.set("connexion ok") 
 
-# fonction qui permet d'activer ou désactiver le bouton btn_next
-def changeState():
-    if (btn_next['state'] == NORMAL):
-        btn_next['state'] = DISABLED
-    else:
-        btn_next['state'] = NORMAL
+def inseeCode():
+    fileInsee = 'C:/Users/david/Desktop/ProjetStage/communes2020.csv'
+    Lecture = csv.reader(open(fileInsee, "r", encoding='utf-8'), delimiter=',')
+    for row in Lecture:
+        CodeInsee = row[1], row[7]
+        print(CodeInsee)
+        
+    
+
 
 # fonction pour afficher la deuxième fenetre
 def NewWindow():
@@ -37,23 +39,19 @@ def NewWindow():
     fenetre.destroy() # détruit la première fenetre
 
     # bouton
-    btn_agreger = Button(newWindow, text='Agréger', bg='black', fg='white')
-    btn_agreger.pack()
-    btn_agreger.place(relx=0.5, rely=0.5, anchor=CENTER)
+    btn = Button(newWindow, text='test', bg='black', fg='white', command= lambda: inseeCode())
+    btn.pack()
+    btn.place(relx=0.5, rely=0.5, anchor=CENTER)
 
 # texte qui affiche à l'utilisateur si il est connecté ou non
 barre = Label(fenetre, textvariable=CheminFile)
 barre.pack()
 barre.place(relx=0.5, rely=0.5, anchor=CENTER)
 
-# bouton pour se connecter 
-btn_recherche = Button(fenetre, text='Connexion', bg='black', fg='white', command=lambda : [changeState(), dsn() ])
-btn_recherche.pack()
-btn_recherche.place(relx=0.5, rely=0.55, anchor=CENTER)
-
 # bouton pour passer à la deuxième fenetre
-btn_next = Button(fenetre, text='Suivant', bg='black', fg='white', state = DISABLED, command= lambda: NewWindow())
+btn_next = Button(fenetre, text='Suivant', bg='black', fg='white', command= lambda: NewWindow())
 btn_next.pack()
 btn_next.place(relx=0.8, rely=0.9)
 
 fenetre.mainloop()
+conn.close()
