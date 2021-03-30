ddc3baa76716ff7e3b468135f057a45aaa0dfbc0
Macrelle_T <65347962+FlowWars@users.noreply.github.com>
2021-03-30 17:04:07 +0200

    commit du code
    - extraction des codes insee et insertion dans un tab

--- projetStage.py
+++ projetStage.py
@@ -19,37 +19,48 @@ conn = pypyodbc.connect('DSN=GDR;')  # initialisation de la connexion au serveur
 cur = conn.cursor()
 CheminFile.set("connexion ok") 
 
-def inseeCode():
-    fileInsee = 'C:/Users/david/Desktop/ProjetStage/communes2020.csv'
+def insee():
+    CodeInsee = []
+
+    # récupération des code communes de l'insee
+    fileInsee = 'C:/Users/david/Desktop/ProjetStage/communes2020.csv' # chemin du .csv (à changer par rapport à la date)
     Lecture = csv.reader(open(fileInsee, "r", encoding='utf-8'), delimiter=',')
     for row in Lecture:
-        CodeInsee = row[1], row[7]
-        print(CodeInsee)
-        
-    
+        ligne = row[1], row[7]
+        CodeInsee.append(ligne) # insertion de la colonne code et nom de commune dans un tableau
 
 
 # fonction pour afficher la deuxième fenetre
-def NewWindow():
+def Agregation():
     newWindow = Tk()
-    newWindow.title('Appli')
+    newWindow.title('Agrégation')
     newWindow.geometry('600x600')
     newWindow.configure(bg='#36A679')
     newWindow.minsize(480,360)
     fenetre.destroy() # détruit la première fenetre
+    
+    insee()
+
+    """     
+    TextValidationInsee = Text(newWindow)
+    TextValidationInsee.insert(1.0, )
+    TextValidationInsee.pack()
+    TextValidationInsee.place(relx=0.5, rely=0.5, anchor=CENTER)
+    """
 
     # bouton
-    btn = Button(newWindow, text='test', bg='black', fg='white', command= lambda: inseeCode())
+    btn = Button(newWindow, text='Suivant', bg='black', fg='white')
     btn.pack()
-    btn.place(relx=0.5, rely=0.5, anchor=CENTER)
+    btn.place(relx=0.8, rely=0.9, anchor=CENTER)
 
+    
 # texte qui affiche à l'utilisateur si il est connecté ou non
 barre = Label(fenetre, textvariable=CheminFile)
 barre.pack()
 barre.place(relx=0.5, rely=0.5, anchor=CENTER)
 
 # bouton pour passer à la deuxième fenetre
-btn_next = Button(fenetre, text='Suivant', bg='black', fg='white', command= lambda: NewWindow())
+btn_next = Button(fenetre, text='Suivant', bg='black', fg='white', command= lambda: Agregation())
 btn_next.pack()
 btn_next.place(relx=0.8, rely=0.9)
 
