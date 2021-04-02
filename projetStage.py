fbc65799feedd622fdacebb899280b27e732a9af
Macrelle_T <65347962+FlowWars@users.noreply.github.com>
2021-04-02 12:40:07 +0200

    insertion des codes insee

--- projetStage.py
+++ projetStage.py
@@ -7,54 +7,38 @@ import sqlite3
 #-----------------------------------------------------------------------------------------#
 #fontions principales:
 
-# fonction pour récupérer les données utiles de l'insee
-def insee():
-    CodeInsee = {} 
+# fonction pour créer les tables de la BDD
+def CreateTable():
 
-    # récupération des code communes de l'insee
-    print('Lecture des codes insee')
-    fileInsee = 'C:/Users/david/Desktop/ProjetStage/communes2020.csv' # chemin du document de l'insee (.csv le format)
-
-    Lecture = csv.reader(open(fileInsee, "r", encoding='utf-8'), delimiter=',')
-    for row in Lecture:
-        Code = row[1] # on récupère les code de l'insee
-        Comm = row[7] # on récupère les communes de l'insee
-        CodeInsee[Comm] = Code # insertion des codes dans le dictionnaire par rapport à la commune
-
-    print('Lecture terminé\n')
-    return CodeInsee
-
-def CreateDatabase():
-
-    try:
-        connect = sqlite3.connect('C:/Users/david/Desktop/ProjetStage/test.db') # connexion à la database
-        curSQL = connect.cursor()
-        print("Base de données connectée à SQLite")
-    
-        CreateTableVente = """CREATE TABLE if not exists Vente (
-                                Id_vente INTEGER NOT NULL PRIMARY KEY,
-                                Id_Produits INTEGER NOT NULL,
-                                Commune_orig TEXT,
-                                Code_postal_orig TEXT,
-                                Code_insee INTEGER NOT NULL,
+    CreateTableVente = """CREATE TABLE if not exists Vente (
+                                Id_Vente INTEGER NOT NULL PRIMARY KEY,
+                                Id_Lots INTEGER NOT NULL,
+                                Commune_Orig TEXT,
+                                Code_Postal_orig TEXT,
+                                Id_Insee INTEGER NOT NULL,
                                 Flux TEXT,
                                 Categorie TEXT,
-                                Sous_categorie TEXT,
+                                Sous_Categorie TEXT,
                                 Montant_HT REAL,
                                 Montant_TTC REAL,
-                                Date_vente DATE,
-                                CONSTRAINT Id_produits_fk FOREIGN KEY(Id_Produits) REFERENCES Produits(Id_Produits)
+                                Date_Vente DATE,
+                                CONSTRAINT Id_lots_fk FOREIGN KEY(Id_Lots) REFERENCES Lots(Id_Lots),
+                                CONSTRAINT Id_insee_fk FOREIGN KEY(Id_Insee) REFERENCES Lots(Id_Insee)
                             );"""
 
-        CreateTableLigne_Vente = """CREATE TABLE if not exists Ligne_Vente (
-                                Id_collecte INTEGER NOT NULL,
-                                Id_vente INTEGER NOT NULL,
-                                CONSTRAINT Id_collecte_fk FOREIGN KEY(Id_collecte) REFERENCES Collecte(Id_collecte),
-                                CONSTRAINT Id_vente_fk FOREIGN KEY(Id_vente) REFERENCES Vente(Id_vente)
+    CreateTableLigne_Vente = """CREATE TABLE if not exists Ligne_Vente (
+                                Id_Ligne_Vente INTEGER NOT NULL PRIMARY KEY,
+                                Id_Produit INTEGER NOT NULL,
+                                Id_Vente INTEGER NOT NULL,
+                                Date_Vente DATE,
+                                Montant_HT REAL,
+                                Montant_TTC REAL,
+                                CONSTRAINT Id_collecte_fk FOREIGN KEY(Id_Collecte) REFERENCES Collecte(Id_Collecte),
+                                CONSTRAINT Id_vente_fk FOREIGN KEY(Id_Vente) REFERENCES Vente(Id_Vente)
                             );"""
 
-        CreateTableCollecte = """CREATE TABLE if not exists Collecte (
-                                Id_collecte INTEGER NOT NULL PRIMARY KEY,
+    CreateTableCollecte = """CREATE TABLE if not exists Collecte (
+                                Id_Produit INTEGER NOT NULL PRIMARY KEY,
                                 Date_arrivage DATE,
                                 Orig_arrivage TEXT,
                                 Code_insee INTEGER NOT NULL,
@@ -65,57 +49,43 @@ def CreateDatabase():
                                 Poids REAL,
                                 Affectation TEXT
                             );""" # affectation = orientation (rebuts, valorisé, ...)
+
+    CreateTableInsee = """CREATE TABLE if not exists Code_Insee (
+                                Id_Insee INTEGER NOT NULL PRIMARY KEY,
+                                Code INTEGER,
+                                Commune TEXT
+                            );"""                       
                                 
-        CreateTableProduit = """CREATE TABLE if not exists Produits (
-                                Id_Produits INTEGER NOT NULL PRIMARY KEY,
+    CreateTableLot = """CREATE TABLE if not exists Lots (
+                                Id_Lots INTEGER NOT NULL PRIMARY KEY,
+                                Id_Ligne_Vente INTEGER NOT NULL,
                                 Nbr_produit INTEGER,
                                 Poids_produits REAL
                             );"""                                             
 
-        curSQL.execute(CreateTableVente)
-        curSQL.execute(CreateTableLigne_Vente)
-        curSQL.execute(CreateTableCollecte)
-        curSQL.execute(CreateTableProduit)
+    #curSQL.execute(CreateTableVente)
+    #curSQL.execute(CreateTableLigne_Vente)
+    #curSQL.execute(CreateTableCollecte)
+    #curSQL.execute(CreateTableLot)
+    curSQL.execute(CreateTableInsee)
 
+# fonction pour récupérer et insérer les codes insee dans la BDD
+def InsertionInsee():
 
-        curSQL.close()
-        conn.close()
-        print("La connexion SQLite est fermée")
-    except sqlite3.Error as error:
-        print("Erreur lors de la connexion à SQLite", error)
+    print('\nLecture des codes insee...')
+    fileInsee = 'C:/Users/david/Desktop/ProjetStage/communes2020.csv' # chemin du document de l'insee (.csv le format)
 
+    Lecture = csv.reader(open(fileInsee, "r", encoding='utf-8'), delimiter=',')
+    next(Lecture, None) # on retire l'en-tete
+    for row in Lecture:
+        Code = row[1] # on récupère les code de l'insee
+        Comm = row[7] # on récupère les communes de l'insee
+        curSQL.execute('INSERT INTO Code_Insee (Code, Commune) VALUES (?, ?)', (Code, Comm)) # insertion des codes dans le dictionnaire par rapport à la commune
+   
+    print('Lecture terminé\n')
 
-"""
-def VerificationCritere(nom):
-    File = xlrd.open_workbook('C:/Users/david/Desktop/ProjetStage/%s.xls' % nom)
-
-    test = File.sheet_by_name('Collecte')
-
-    # Col? =(test.col(?))
-"""
-
-# a voir a la fin pour l'excel
-"""
-def Excel(nom):
-    FileExcel = xlwt.Workbook()
-    FeuilleCollect = FileExcel.add_sheet('Collecte')
-
-    # tableau des titres de collecte
-    ColTitreCollect = ["Identifiant unique de la vente","Commune d'origine du client","Code postal de la commune d'origine", "code insee","Flux", "Catégorie", "Sous-Catégorie", "Nombre de produits", "Poids des produits", "Montant HT", "Montant TTC", "IDProduit"]
-    
-    for i in range(len(ColTitreCollect)):
-        FeuilleCollect.write(1,i,ColTitreCollect[i])
-
-    FeuilleVente = FileExcel.add_sheet('Vente')
-
-    # tableau des titres de vente
-    ColTitreVente = ["id produit", "id arrivage", "Date arrivage", "Origine arrivage", "id arrivage", "Code insee", "Flux", "Catégorie", "Sous-Catégorie", "Quantité", "Poids", "Affectation"]
-
-    for i in range(len(ColTitreVente)):
-        FeuilleVente.write(1,i,ColTitreVente[i])
-
-    FileExcel.save('C:/Users/david/Desktop/ProjetStage/%s.xls' % nom) # chemin pour sauvegarder le fichier
-"""
+#--------------------------------------------------------------------------------------------------------------
+# Code principal
 
 NameBase = 'GDR' # nom de votre base ODBC
 
@@ -124,8 +94,20 @@ conn = pypyodbc.connect(DSN=NameBase)  # initialisation de la connexion au serve
 curGDR = conn.cursor()
 print("connexion ok\n") 
 
-Code = insee() # récupération des codes dans une variable
-CreateDatabase()
+try:
+    connect = sqlite3.connect('C:/Users/david/Desktop/ProjetStage/test.db') # connexion à la database
+    curSQL = connect.cursor()
+    print("Base de données connectée à SQLite")
+    
+    CreateTable()
+
+    InsertionInsee()
+
+    curSQL.close()
+    conn.close()
+    print("La connexion SQLite est fermée")
+except sqlite3.Error as error:
+    print("Erreur lors de la connexion à SQLite", error)
 
 # print(Code['Froissy']) 
 
