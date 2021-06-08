f40ceecf29687b97b6d28800849817d104c63146
Louis-Mont <Louis-Mont@users.noreply.github.com>
2021-06-08 17:05:56 +0200

    Added tests

---
+++ tests/test_State.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+
+class TestState(TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestState, self).__init__(*args, **kwargs)
+
+    def test_config(self):
+        self.fail()
+
+    def test_configs(self):
+        self.fail()
+
+    def test__state(self):
+        self.fail()
