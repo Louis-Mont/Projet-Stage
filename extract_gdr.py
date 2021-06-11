fa3cd640428c593c4b4ad07f78f8537dd3c58d5e
Louis-Mont <Louis-Mont@users.noreply.github.com>
2021-06-11 16:03:17 +0200

    Added db_sql

--- extract_gdr.py
+++ extract_gdr.py
@@ -1,6 +1,7 @@
 from tkinter import Tk
 
 from Ui.Structure import Structure
+from database_interface.DatabaseSQLite import DatabaseSQLite
 
 
 def next_window():
@@ -11,3 +12,5 @@ if __name__ == '__main__':
     struct_frame = Tk()
     extract_frame = Tk()
     struct = Structure(struct_frame, lambda: next_window(), )
+    db_sql = DatabaseSQLite()
+
