cbb897f8a0074740c0d28615aae338a7e7957ce1
Louis-Mont <Louis-Mont@users.noreply.github.com>
2021-06-18 17:25:52 +0200

    Added get_cat, get_origin, get_min_y Refactored the usage of the DB Started next_window implementation

--- extract_gdr.py
+++ extract_gdr.py
@@ -1,5 +1,5 @@
 from tkinter import Tk
-
+from user_interface.Extract import Extract
 from user_interface.Structure import Structure
 from database_interface.DatabaseSQLite import DatabaseSQLite
 
@@ -7,15 +7,53 @@ from database_interface.DatabaseSQLite import DatabaseSQLite
 def get_rec(db):
     """
     :param db: the sqlite database you're looking into
+    :type db: DatabaseSQLite
     :return: List of Recycleries
+    :rtype: list
     """
-    cur = db.cursor()
-    cur.execute("SELECT Recyclerie FROM Organisation")
-    return [row[0] for row in cur.fetchall()]
+    return [row[0] for row in db.sf('Organisation', ['Recyclerie'])]
 
 
-def next_window():
-    pass
+def get_cat(db):
+    """
+    :param db: the sqlite database you're looking into
+    :type db: DatabaseSQLite
+    :return: List of Categories
+    :rtype: list
+    """
+    return [row[0] for row in db.sf('Catégorie', ['Catégorie'])]
+
+
+def get_origin(db):
+    """
+    :param db: the sqlite database you're looking into
+    :type db: DatabaseSQLite
+    :return: List of Origins
+    :rtype: list
+    """
+    return [row[0] for row in db.sf('Arrivage', ['origine'], True) if row[0] != 0]
+
+
+def get_min_y(db):
+    """
+    :param db: the sqlite database you're looking into
+    :type db: DatabaseSQLite
+    :return: minimum year in the DB
+    :rtype: int
+    """
+    return db.sf('Arrivage', ["Min(date)"])[0][0][:4]
+
+
+def next_window(frame, gdr_db):
+    """
+    :type frame: Tk
+    :type gdr_db: DatabaseSQLite
+    """
+    frame.destroy()
+    min_year = get_min_y(gdr_db)
+    ext = Extract(frame, min_year, None, None, get_cat(gdr_db), get_origin(gdr_db))
+
+    frame.mainloop()
 
 
 if __name__ == '__main__':
@@ -30,6 +68,6 @@ if __name__ == '__main__':
         raise FileNotFoundError(cr[0])
     print(cr[0])
 
-    struct = Structure(frame, lambda: next_window(), get_rec(db_sql.DB))
+    struct = Structure(frame, lambda: next_window(frame, db_sql), get_rec(db_sql))
 
     frame.mainloop()
